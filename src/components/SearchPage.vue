cc6208a2d137b87ecfe9f86090a1002bb82d2350
rais <rais@raiss-MacBook-Pro.local>
2022-07-24 00:53:04 +0000

    added search data to search item page

--- src/components/SearchPage.vue
+++ src/components/SearchPage.vue
@@ -28,7 +28,7 @@
              <div class="w-[10%]"></div>
          <div class="group">
     <a href="#" class="">All</a>
-    <div class="border border-b-1 border-black opacity-0 group-hover:opacity-100"></div>
+    <div class="border border-b-1 border-black  group-hover:opacity-100"></div>
      </div>
       <div class="group">
     <a href="#" class="">Videos</a>
@@ -52,9 +52,25 @@
      </div>
       </div>
       <!-- divided -->
-      <div class="border  border-[#9aa0a6]">
-
+      <div class="border  border-[#9aa0a6]"></div>
+      <div class="flex">
+        <div class="w-[10%]"></div>
+      <div class="flex flex-col text-[#9aa0a6] mt-2 mx-2 space-y-2">
+            <span>About 10,1000,100 results (0.55 seconds)</span>
+            <div class="flex flex-col" v-for="search_item in search_items" :key="search_item.id">
+            <a :href="search_item.link">{{search_item.link}}</a>
+            <div class="text-blue-300 font-medium">
+             {{search_item.title}}
+            </div>
+            <p>{{search_item.created_at}} 7 answers</p>
+            <div class="text-blue-300 font-medium">
+             {{search_item.short_desc}}
+            </div>
+        </div>
+        </div>
+        
       </div>
+      
     </div>
 </template>
 
@@ -64,12 +80,15 @@ export default{
     data(){
         return {
          query_data: '',
-         search_text: ''
+         search_text: '',
+         search_items: []
         }
     },
    created() {
     this.query_data = this.$route.query.q
     this.search_text = this.$route.query.q
+    console.log(this.$store.state.search_items)
+    this.search_items = this.$store.state.search_items
    
    }
 }
--- src/store.js
+++ src/store.js
@@ -10,48 +10,64 @@ const store = createStore({
             counter: '04',
             search_items:[
                 {
+                    id: 1,
                     link:'https//test.com',
+                    title: 'Get the Latest',
                     created_at: '28 Dec 2017',
                     short_desc:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
                     
                 },
                 {
+                    id: 2,
+                    title: 'Machine Learning Examples',
                     link:'https//test.com',
                     created_at: '8 jan 2002',
                     short_desc:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
                 
                 },
                 {
+                    id: 3,
+                    title: 'BeanStlk AWS lambda',
                     link:'https//test.com',
                     created_at: '28 Dec 2017',
                     short_desc:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
                 
                 },
                 {
+                    id: 4,
                     link:'https//test.com',
+                    title: 'Vue js and Reactive(Differences)',
                     created_at: '28 Dec 2017',
                     short_desc:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
                 
                 },
                 {
+                    id: 5,
+                    title: 'Introduction to Programming',
                     link:'https//test.com',
                     created_at: '28 Dec 2017',
                     short_desc:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
                 
                 }
                 ,{
+                    id: 6,
+                    title: 'How machine learning works',
                     link:'https//test.com',
                     created_at: '28 Dec 2017',
                     short_desc:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
                 
                 },
                 {
+                    id: 7,
+                    title: 'Youtube tutorials on desktop application',
                     link:'https//test.com',
                     created_at: '8 april 2017',
                     short_desc:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
                 
                 },
                 {
+                    id: 8,
+                    title: 'More Example on Text classification',
                     link:'https//test.com',
                     created_at: '14 oct 2019',
                     short_desc:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
