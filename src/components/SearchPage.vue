c2664e6a95021ec23583093be5e689f07616bbc5
rais <rais@raiss-MacBook-Pro.local>
2022-07-24 00:13:02 +0000

    modified search page

--- src/components/SearchPage.vue
+++ src/components/SearchPage.vue
@@ -1,6 +1,61 @@
 <template>
-    <div>this is a test again now {{$store.state.counter}}</div>
-     <div>this is a test {{$store.state.data}}</div>
+    <div class="flex mx-2 mt-6">
+        <div class="flex space-x-4 flex-1">
+            <img src="../../public/google.png" class="w-[10%]"/>
+            <input type="text" class="rounded-full w-1/2 py-4 outline outline-1 outline-[#dfe1e5] hover:drop-shadow-md px-2" v-model="search_text">
+
+        </div>
+         <div class="flex space-x-3 justify-end items-center m-4">
+      <div class="group">
+    <a href="#" class="">Gmail</a>
+    <div class="border border-b-1 border-black opacity-0 group-hover:opacity-100"></div>
+     </div>
+     <div class="group">
+    <a href="#" class="">Email</a>
+    <div class="border border-b-1 border-black opacity-0 group-hover:opacity-100"></div>
+     </div>
+  
+    <div class="rounded-full bg-green-900 w-10 h-10 flex flex-col items-center justify-center">
+     <div class="rounded-full bg-green-900 w-6 h-6 border-4 border-white"></div>
+    </div>
+    </div>
+    </div>
+    <!-- search section -->
+    <div class="flex flex-col mx-2 mt-6">
+      <!-- tabs -->
+      <div class="flex space-x-3 items-center">
+             <!-- move everything to be inline with the search items -->
+             <div class="w-[10%]"></div>
+         <div class="group">
+    <a href="#" class="">All</a>
+    <div class="border border-b-1 border-black opacity-0 group-hover:opacity-100"></div>
+     </div>
+      <div class="group">
+    <a href="#" class="">Videos</a>
+    <div class="border border-b-1 border-black opacity-0 group-hover:opacity-100"></div>
+     </div>
+      <div class="group">
+    <a href="#" class="">News</a>
+    <div class="border border-b-1 border-black opacity-0 group-hover:opacity-100"></div>
+     </div>
+      <div class="group">
+    <a href="#" class="">Images</a>
+    <div class="border border-b-1 border-black opacity-0 group-hover:opacity-100"></div>
+     </div>
+      <div class="group">
+    <a href="#" class="">Maps</a>
+    <div class="border border-b-1 border-black opacity-0 group-hover:opacity-100"></div>
+     </div>
+      <div class="group">
+    <a href="#" class="">More</a>
+    <div class="border border-b-1 border-black opacity-0 group-hover:opacity-100"></div>
+     </div>
+      </div>
+      <!-- divided -->
+      <div class="border  border-[#9aa0a6]">
+
+      </div>
+    </div>
 </template>
 
 <script>
@@ -8,11 +63,13 @@
 export default{
     data(){
         return {
-         query_data: ''
+         query_data: '',
+         search_text: ''
         }
     },
    created() {
     this.query_data = this.$route.query.q
+    this.search_text = this.$route.query.q
    
    }
 }
--- src/store.js
+++ src/store.js
@@ -7,7 +7,57 @@ const store = createStore({
     },
     state(){
         return {
-            counter: '04'
+            counter: '04',
+            search_items:[
+                {
+                    link:'https//test.com',
+                    created_at: '28 Dec 2017',
+                    short_desc:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+                    
+                },
+                {
+                    link:'https//test.com',
+                    created_at: '8 jan 2002',
+                    short_desc:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+                
+                },
+                {
+                    link:'https//test.com',
+                    created_at: '28 Dec 2017',
+                    short_desc:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+                
+                },
+                {
+                    link:'https//test.com',
+                    created_at: '28 Dec 2017',
+                    short_desc:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+                
+                },
+                {
+                    link:'https//test.com',
+                    created_at: '28 Dec 2017',
+                    short_desc:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+                
+                }
+                ,{
+                    link:'https//test.com',
+                    created_at: '28 Dec 2017',
+                    short_desc:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+                
+                },
+                {
+                    link:'https//test.com',
+                    created_at: '8 april 2017',
+                    short_desc:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+                
+                },
+                {
+                    link:'https//test.com',
+                    created_at: '14 oct 2019',
+                    short_desc:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+                
+                }
+            ],
         }
     }
 })
